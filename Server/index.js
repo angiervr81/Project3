d9d54b97cbec2600786abd8746b5a64452d3c064
angiervr81 <Angie.Rivera81@bcmail.cuny.edu>
2024-08-10 23:45:32 -0400

    package.json

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require (socket.io);
+
+const app= express ();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('client'));
+
+io.on('connection', (socket)=>{
+    console.log('A user connected');
+
+    socket.on('chat message', (msg)=>{
+        io.emit('chat message',msg);
+    });
+});
+
+const PORT =3000;
+server.listen(PORT, ()=>{
+    console.log('Sever is running on http://localhost:${PORT}');
+});
