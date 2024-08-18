3442ca2468670be7ba9ac3e686c835b2a585d750
Angie <Angie.Rivera81@bcmail.cuny.edu>
2024-08-17 23:37:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+//http://localhost:3000
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
