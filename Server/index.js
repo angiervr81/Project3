8e0a4ab4c394bed895641bdfc93ec3e6be05646a
Angie <Angie.Rivera81@bcmail.cuny.edu>
2024-08-17 20:31:38 -0400

    Recreating a new server

--- Server/index.js
+++ Server/index.js
@@ -1,84 +1,10 @@
+const path = require('path');
 const express = require('express');
-const http = require('http');
-const socketIo = require ('socket.io');
 
-const app= express ();
-const server = http.createServer(app);
-const io = socketIo(server);
+const app = express(); 
+// Set static folder 
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use(express.static('client'));
+const PORT = 3000 || process.env.PORT;
 
-//store user details in memory
-const users ={};
-const rooms ={}; // room messgae histories
-
-// handle new connections
-io.on('connection', (socket)=>{
-    console.log('A user connected');
-
-    // register a new user 
-    socket.on('register',(username) =>{
-        users[username] = socket.id;
-        console.log('${username} connected with socket ID:${socket.id}');
-        io.emit('user list', Object.keys(users)); // updating list of users
-    });
-
-// handle joing chat room
-socket.on('join room',(room) =>{
-    socket.join(room);
-    console.log('User ${socket.id} joined room ${room}');
-
-    if(room[room]){
-        socket.emit('chat history', rooms[room]);
-    }
-});
-
-//leaving room
-
-socket.on('leaving room',(room) =>{
-    socket.leave(room);
-    console.log('User ${socket.id} left room ${room}');
-});
-
-
-    // handling sending a message to room
-    socket.on('room message', (data)=>{
-        const {room,message}=data;
-
-        io.to(room).emit('room message',{message, sender:socket.id});
-
-        if(!room[room]){
-            rooms[room] =[];
-        }
-
-        rooms[room].push({message, sender:socket.io});
-    });
-
-    // handling private messages 
-    socket.on('private message', (data) =>{
-        const {recipient, message} = data;
-        const recipientSocketID = users[recipient];
-        if(recipientSocketID){
-            io.to(recipientSocketID).emit('private message', {message, sender:socket.id});
-        }else{
-            socket.emit('private message', {message:'User not found', sender: null});
-        }
-    });
-
-    socket.on('disconnect',() =>{
-        console.log('User disconnected');
-
-        for (const [username, id] of Object.entries(users)){
-            if(id === socket.id){
-                delete users[username];
-                io.emit('user list', Object.keys(users));
-                break; 
-            }
-        }
-    })
-});
-
-const PORT =3000;
-server.listen(PORT, ()=>{
-    console.log('Sever is running on http://localhost:${PORT}');
-});
+app.listen(PORT, () => console.log('Server running on port $(Port)'));
